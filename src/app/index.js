8c3cd31dcfbf9b62bfb0ab874ebd98720d318ccb
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-03-23 20:52:00 +0800

    added flatlist

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import FoodListItem from '../components/FootListItem';
 
+const foodItems = [
+  {label: 'Pizza', cal: 75, brand: 'Dominos'},
+  {label: 'Apple', cal: 45, brand: 'Farmer'},
+  {label: 'Coffee', cal: 40, brand: 'Americano'},
+  {label: 'Beer', cal: 120, brand: 'Snow'}
+];
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FoodListItem item={{label: 'Pizza', cal: 75, brand: 'Dominos'}}/>
-      <FoodListItem item={{label: 'Apple', cal: 45, brand: 'Farmer'}}/>
+      <FlatList
+        data={foodItems}
+        renderItem ={({item}) => <FoodListItem item={item}/>}
+        contentContainerStyle={{gap: 5}}
+      />
     </View>
   );
 }
@@ -16,8 +25,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
     padding: 10,
-    gap:5,
   },
 });
